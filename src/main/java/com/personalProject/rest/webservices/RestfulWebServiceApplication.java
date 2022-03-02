79ca8d90228494785e82f28e94a50aab07c8ca38
ShubhamKumar1996 <Nimcetshubhamkumar@gmail.com>
2022-03-02 08:43:11 +0530

    Added UserResource (endpoints)

--- src/main/java/com/personalProject/rest/webservices/RestfulWebServiceApplication.java
+++ src/main/java/com/personalProject/rest/webservices/RestfulWebServiceApplication.java
@@ -1,4 +1,4 @@
-package com.personalProject.rest.webservices.restfulwebservice;
+package com.personalProject.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
